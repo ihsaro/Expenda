1d900b99abbe6f342f719d047b28876a35ff6712
Idjaz Hossanee <idjazhossanee@gmail.com>
2023-07-13 19:18:02 +0400

    add summary to generic repository.

--- src/Expenda.Domain/Repositories/IGenericRepository.cs
+++ src/Expenda.Domain/Repositories/IGenericRepository.cs
@@ -4,12 +4,59 @@ namespace Expenda.Domain.Repositories;
 
 public interface IGenericRepository<T> where T : BaseEntity
 {
+    /// <summary>
+    /// Creates entities of generic type T into the repository context
+    /// </summary>
+    /// <param name="entities">Entities to be created</param>
     void BatchCreate(IEnumerable<T> entities);
+
+    /// <summary>
+    /// Creates entity of generic type T into the repository context
+    /// </summary>
+    /// <param name="entity">Entity to be created</param>
     void Create(T entity);
+
+    /// <summary>
+    /// Deletes entity of generic type T from the repository context
+    /// </summary>
+    /// <param name="entity">Entity to be deleted</param>
     void Delete(T entity);
+
+    /// <summary>
+    /// Deletes entities of generic type T from the repository context
+    /// </summary>
+    /// <param name="entities">Entities to be deleted</param>
     void BatchDelete(IEnumerable<T> entities);
+
+    /// <summary>
+    /// Updates an entity
+    /// </summary>
+    /// <param name="entity">Entity to be updated</param>
     void Update(T entity);
+
+    /// <summary>
+    /// Gets all entities of generic type T, while not loading the related entities within,
+    /// so overriding this method is recomended in case related entities need to be loaded.
+    /// </summary>
+    /// <param name="take">Number of entities to be loaded (Default: 1000)</param>
+    /// <param name="skip">Number of entities to be skipped (Default: 0)</param>
+    /// <param name="token">Cancellation token</param>
+    /// <returns>IEnumerable of entities of type T</returns>
     Task<IEnumerable<T>> GetAll(int take = 1000, int skip = 0, CancellationToken token = default);
+
+    /// <summary>
+    /// Gets 1 entity of generic type T based on an id, while not loading the related entities within,
+    /// so overriding this method is recomended in case related entities need to be loaded.
+    /// </summary>
+    /// <param name="id">Id of the entity to be fetched</param>
+    /// <param name="token">Cancellation token</param>
+    /// <returns>Entity of type T</returns>
     Task<T?> GetById(int id, CancellationToken token = default);
+
+    /// <summary>
+    /// Commits every transaction from the context to the database.
+    /// </summary>
+    /// <param name="token">Cancellation token</param>
+    /// <returns>Number of records affected</returns>
     Task<int> Commit(CancellationToken token = default);
 }
--- src/Expenda.Infrastructure/Persistence/Repositories/GenericRepository.cs
+++ src/Expenda.Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -14,22 +14,68 @@ internal class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
         _context = context;
         Table = _context.Set<T>();
     }
-    
-    public virtual void BatchCreate(IEnumerable<T> entities) => Table.AddRange(entities);
 
-    public virtual void Create(T entity) => Table.Add(entity);
+    /// <summary>
+    /// Creates entities of generic type T into the repository context
+    /// </summary>
+    /// <param name="entities">Entities to be created</param>
+    public virtual void BatchCreate(IEnumerable<T> entities)
+        => Table.AddRange(entities);
 
-    public virtual void Delete(T entity) => Table.Remove(entity);
-    
-    public virtual void BatchDelete(IEnumerable<T> entities) => Table.RemoveRange(entities);
+    /// <summary>
+    /// Creates entity of generic type T into the repository context
+    /// </summary>
+    /// <param name="entity">Entity to be created</param>
+    public virtual void Create(T entity)
+        => Table.Add(entity);
 
-    public virtual void Update(T entity) => Table.Update(entity);
+    /// <summary>
+    /// Deletes entity of generic type T from the repository context
+    /// </summary>
+    /// <param name="entity">Entity to be deleted</param>
+    public virtual void Delete(T entity)
+        => Table.Remove(entity);
 
+    /// <summary>
+    /// Deletes entities of generic type T from the repository context
+    /// </summary>
+    /// <param name="entities">Entities to be deleted</param>
+    public virtual void BatchDelete(IEnumerable<T> entities)
+        => Table.RemoveRange(entities);
+
+    /// <summary>
+    /// Updates an entity
+    /// </summary>
+    /// <param name="entity">Entity to be updated</param>
+    public virtual void Update(T entity)
+        => Table.Update(entity);
+
+    /// <summary>
+    /// Gets all entities of generic type T, while not loading the related entities within,
+    /// so overriding this method is recomended in case related entities need to be loaded.
+    /// </summary>
+    /// <param name="take">Number of entities to be loaded (Default: 1000)</param>
+    /// <param name="skip">Number of entities to be skipped (Default: 0)</param>
+    /// <param name="token">Cancellation token</param>
+    /// <returns>IEnumerable of entities of type T</returns>
     public virtual async Task<IEnumerable<T>> GetAll(int take = 1000, int skip = 0, CancellationToken token = default)
-        => await Table.Skip(skip).Take(take).ToListAsync();
+        => await Table.Skip(skip).Take(take).ToListAsync(token);
 
+    /// <summary>
+    /// Gets 1 entity of generic type T based on an id, while not loading the related entities within,
+    /// so overriding this method is recomended in case related entities need to be loaded.
+    /// </summary>
+    /// <param name="id">Id of the entity to be fetched</param>
+    /// <param name="token">Cancellation token</param>
+    /// <returns>Entity of type T</returns>
     public virtual async Task<T?> GetById(int id, CancellationToken token = default)
         => await Table.FirstOrDefaultAsync(x => x.Id == id, token);
 
-    public virtual async Task<int> Commit(CancellationToken token = default) => await _context.SaveChangesAsync(token);
+    /// <summary>
+    /// Commits every transaction from the context to the database.
+    /// </summary>
+    /// <param name="token">Cancellation token</param>
+    /// <returns>Number of records affected</returns>
+    public virtual async Task<int> Commit(CancellationToken token = default)
+        => await _context.SaveChangesAsync(token);
 }
