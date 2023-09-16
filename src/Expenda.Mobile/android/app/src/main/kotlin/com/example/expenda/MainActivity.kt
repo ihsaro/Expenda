05a3ea1eb19b6f19468fe003cf47d0fc785dec14
Idjaz Hossanee <idjazhossanee@gmail.com>
2023-09-16 13:07:00 +0400

    REST and Mobile layers scrapped.

--- src/Expenda.Mobile/android/app/src/main/kotlin/com/example/expenda/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.expenda
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- src/Expenda.REST/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from fastapi.security import OAuth2PasswordBearer
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/v1/user/login")
-
-app = FastAPI()
-
